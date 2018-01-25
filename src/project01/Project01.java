edabba39d39ce0348c8e3daa571f42c6e77a194d
Mathew Horner <mathewhorner123@gmail.com>
2018-01-25 11:09:53 -0600

    First commit.

---
+++ src/project01/Project01.java
@@ -0,0 +1,12 @@
+/*
+ * Authors: Mathew Horner and Nick Morrow
+ * Project: Algorithms Project 1.
+ */
+package project01;
+
+public class Project01 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
